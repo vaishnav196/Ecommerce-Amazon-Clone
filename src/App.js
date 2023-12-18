6925478cef00eb9c944b374c22400804810dbcc8
vaishnav196 <vaish00721@gmail.com>
2023-12-18 18:53:05 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Navbar from "./Component/Header/Navbar";
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar />
     </div>
   );
 }
--- src/Component/Header/Navbar.jsx
+++ src/Component/Header/Navbar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>navbar here</h1>
+    <div className='Navbar'>
+      
+
     </div>
   )
 }
